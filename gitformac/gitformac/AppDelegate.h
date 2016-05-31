279782f307c008eca89ab589e14a4b767b5905b1
dubianluyi <1083279663@qq.com>
2016-05-31 09:18:18 +0800

    add a new project

---
+++ gitformac/gitformac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitformac
+//
+//  Created by yanyueya on 16/5/31.
+//  Copyright © 2016年 com.yanyueya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
